b575894e9a76d9e8a3a30a0a0f3d10ac10755e66
Chopin85 <p.cata85@gmail.com>
2018-11-29 09:39:45 +0100

    Remove migration and eslint+prettier

--- app.js
+++ app.js
@@ -1,23 +1,13 @@
-const express = require("express");
+const express = require('express');
+
 const app = express();
-<<<<<<< HEAD
 const models = require('./models');
 
-=======
-const models = require("./models");
->>>>>>> 595e7215c38f9e3460d43f1f6f2985372a0cb4b9
-
-app.get("/", function(req, res) {
-    res.send("Hello World");
+app.get('/', (req, res) => {
+  res.send('Hello World');
 });
 
-models.sequelize.sync().then(() => {
-app.listen(process.env.PORT || 3001);
-});
-
-<<<<<<< HEAD
-=======
 models.sequelize.sync().then(() => {
   app.listen(process.env.PORT || 3001);
-})
->>>>>>> 595e7215c38f9e3460d43f1f6f2985372a0cb4b9
+});
+models.exports = app;
