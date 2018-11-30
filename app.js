0b93bd0768f5b24841be06fb846890c530abdea0
Prudence Gaboriau <prudence.gaboriau@gmail.com>
2018-11-30 12:44:52 +0100

    creation folders routers, route post create account form

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
-
 const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const models = require('./models');
+const { traineeRoute } = require('./routers');
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+app.use('/trainee', traineeRoute);
 
 models.sequelize.sync().then(() => {
   app.listen(process.env.PORT || 3001);
---
+++ routers/index.js
@@ -0,0 +1,1 @@
+exports.traineeRoute = require('./traineeRoute');
