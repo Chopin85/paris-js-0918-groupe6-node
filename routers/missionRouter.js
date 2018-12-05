ac8d23596a5ccc2a9241f1013ce55ba231dd5755
samirkhennache <samirkhennache@gmail.com>
2018-12-05 15:24:28 +0100

    data base modified

---
+++ routers/missionRouter.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const models = require('../models');
+
+const missionRouter = express.Router();
+
+missionRouter
+  .route('/')
+  .post((req, res) => {
+    // const { titleMission, dateStart, dateEnd, town, intro, description, pictures } = req.body;
+    const newForm = new models.Missions({
+      titleMission: req.body.titleMission,
+      dateStart: req.body.dateStart,
+      dateEnd: req.body.dateEnd,
+      town: req.body.town,
+      intro: req.body.intro,
+      description: req.body.description,
+      pictures: req.body.pictures
+    });
+    newForm.save();
+    res.end('fin post');
+  })
+  .get((req, res) => {
+    models.Missions.findAll().then(mf => res.send(mf));
+  });
+
+missionRouter
+  .route('/:id(\\d+)')
+
+  /* Route Missions */
+
+  // mf = MissionFound
+
+  .get((req, res) => {
+    models.Missions
+      .find({
+        where: { id: req.params.id }
+      })
+      .then(mf => (mf ? res.json(mf) : res.status(404).json({ error: 'Pas de Mission Men' })));
+  })
+
+  .put((req, res) => {
+    const { titleMission, dateStart, dateEnd, town, intro, description, pictures } = req.body;
+    models.Missions
+      .find({
+        where: {
+          id: req.params.id
+        }
+      })
+      .then(mf => {
+        if (mf) {
+          mf.updateAttributes({
+            titleMission,
+            dateStart,
+            dateEnd,
+            town,
+            intro,
+            description,
+            pictures
+          }).then(mf => {
+            res.send(mf);
+          });
+        } else {
+          res.status(404).json({ error: 'pas de Mission' });
+        }
+      });
+  })
+
+  .delete((req, res) => {
+    models.Missions
+      .destroy({
+        where: {
+          id: req.params.id
+        }
+      })
+      .then(mf => (mf ? res.json(mf) : res.status(404).json({ error: 'Pas de Mission' })));
+  });
+
+// app.use('/mission', router);
+
+module.exports = missionRouter;
