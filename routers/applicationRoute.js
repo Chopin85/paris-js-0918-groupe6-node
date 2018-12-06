d35c4de84a95320bf4038204716c96b8ca8e31ad
samirkhennache <samirkhennache@gmail.com>
2018-12-06 11:49:44 +0100

    delete route created

--- routers/applicationRoute.js
+++ routers/applicationRoute.js
@@ -4,9 +4,22 @@ const Router = express.Router();
 
 const models = require('../models');
 
-// Router.post('/', (req, res) => {
-
-// });
+Router.put('/', (req, res) => {
+  const { MissionId, TraineeId } = req.body;
+  models.Applications.findOne({
+    where: { TraineeId, MissionId, statusAppli: true }
+  }).then(applicationFound => {
+    if (applicationFound) {
+      applicationFound.update({
+        statusAppli: false
+      });
+      // applicationFound.save();
+      res.status(201).json({ messga: 'application deleted' });
+    } else {
+      res.status(404).json({ error: 'application already deleted ' });
+    }
+  });
+});
 Router.get('/', (req, res) => {
   res.status(200).json({ message: 'hello to my world' });
 });
@@ -14,16 +27,6 @@ Router.get('/', (req, res) => {
 Router.post('/', (req, res) => {
   // get id_user and id_mission
   const { MissionId, TraineeId } = req.body;
-  console.log('Id_mission', MissionId);
-  console.log('id_trainee', TraineeId);
-  // const applicationForm = new models.Applications({
-  //   statusAppli: true,
-  //   MissionId,
-  //   TraineeId
-  // });
-  // applicationForm.save();
-  // res.status(200).json({ messga: 'application created' });
-
   models.Applications.findOne({
     where: { TraineeId, MissionId }
   }).then(applicationFound => {
