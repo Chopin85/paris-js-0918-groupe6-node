13b1bfba7123cee58320b618e57c5a57cc28b53c
MohamedWild <kerkeb.moha@gmail.com>
2018-12-18 09:19:01 +0100

    mise a jour de paradata

--- routers/paraData.js
+++ routers/paraData.js
@@ -3,13 +3,8 @@ const models = require('../models');
 
 const paraData = express.Router();
 
-<<<<<<< HEAD
-paraData.route('/').get((req, res) => {
-  models.findAll().then(al => res.send(al));
-=======
 paraData.route('/levelstudies').get((req, res) => {
   models.LevelStudies.findAll().then(al => res.send(al));
->>>>>>> e4effc111e524db1f0925d67fd1692a8ddaf37ca
 });
 
 module.exports = paraData;
